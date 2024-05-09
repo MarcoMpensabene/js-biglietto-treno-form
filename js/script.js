48c4e040d3a6e1df4cb217c21639840619c9edac
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-09 16:36:35 +0200

    added condition for discount and a function that return the final price with the click of the button

--- js/script.js
+++ js/script.js
@@ -11,3 +11,33 @@
 // > La risposta finale (o output) sarà anch’essa da scrivere in console.MILESTONE 2:
 // > Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // > Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+const userKmEl = document.getElementById("userKm").value;
+
+const userAgeEl = document.getElementById("userAge").value;
+
+const priceForKm = 0.267113
+let discount = 0;
+
+if (userAgeEl >= 63){
+    discount = 37.893;
+} else if (userAgeEl <= 21){
+    discount = 24.552;
+}
+
+let price = priceForKm * userKmEl ;
+price = price - ((price / 100) * discount);
+
+let buttonEl = document.querySelector("button");
+
+buttonEl.addEventListener("click" , 
+
+    function() {
+        document.getElementById("price").innerHTML = price.toFixed(2)
+    }
+
+)
+    
+
+
+
+//  ? console.log(price.toFixed(2));
